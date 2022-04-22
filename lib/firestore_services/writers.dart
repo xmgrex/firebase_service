12a480f0e8d794a36caa082e54860d3877033fee
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-04-22 15:46:23 +0900

    add update method edit method name

--- lib/firestore_services/writers.dart
+++ lib/firestore_services/writers.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Writers {
 
-  Future<void> setData({
+  Future<void> set({
     required String path,
     required Map<String, dynamic> data,
     bool merge = false,
@@ -12,7 +12,16 @@ class Writers {
     await reference.set(data, SetOptions(merge: merge));
   }
 
-  Future<void> deleteData({required String path}) async {
+  Future<void> update({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('$path: $data');
+    await reference.update(data);
+  }
+
+  Future<void> delete({required String path}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('delete: $path');
     await reference.delete();
