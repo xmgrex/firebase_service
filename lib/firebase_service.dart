5c87c605244a59d9fa186ff63198ac022c928c72
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-12-10 14:04:11 +0900

    create example

---
+++ lib/firebase_service.dart
@@ -0,0 +1,3 @@
+library firebase_service;
+
+export 'src/service.dart';
---
+++ lib/src/readers.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_service/src/service.dart';
+
+class Readers {
+  
+  DocumentReference document({required String path}) {
+    return FirebaseFirestore.instance.doc(path);
+  }
+
+  CollectionReference collection({required String path}) {
+    return FirebaseFirestore.instance.collection(path);
+  }
+
+  Future<T> getDocument<T>({
+    required String path,
+    required T Function(Map<String, dynamic>? data, String documentID) builder,
+  }) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('path: $path');
+    return await reference
+        .getCacheElseServer<Map<String, dynamic>>()
+        .then((snapshot) {
+      return builder(snapshot.data(), snapshot.id);
+    });
+  }
+
+  Future<List<T>> getCollection<T>({
+    required String path,
+    required T Function(Map<String, dynamic>? data, String documentID) builder,
+    int Function(T lhs, T rhs)? sort,
+  }) async {
+    final reference = FirebaseFirestore.instance.collection(path);
+    print('path: $path');
+    return await reference.getCacheElseServer().then((snapshot) {
+      final result = snapshot.docs
+          .map((snapshot) => builder(snapshot.data(), snapshot.id))
+          .where((value) => value != null)
+          .toList();
+      if (sort != null) {
+        result.sort(sort);
+      }
+      return result;
+    });
+  }
+
+  Future<List<T>> getCollectionLimited<T>({
+    required String path,
+    required int limit,
+    required T Function(Map<String, dynamic>? data, String documentID) builder,
+    int Function(T lhs, T rhs)? sort,
+  }) async {
+    final reference = FirebaseFirestore.instance.collection(path).limit(limit);
+    print('path: $path');
+    return await reference.get().then((snapshot) {
+      final result = snapshot.docs
+          .map((snapshot) => builder(snapshot.data(), snapshot.id))
+          .where((value) => value != null)
+          .toList();
+      if (sort != null) {
+        result.sort(sort);
+      }
+      return result;
+    });
+  }
+}
